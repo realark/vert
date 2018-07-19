6ffa7963e1c4502b8e30079b2897b0cf2c821ed8
Ark <arktheprogrammer@gmail.com>
2018-07-19 08:27:52 -0700

    layered background image

--- src/scene/scene-background.lisp
+++ src/scene/scene-background.lisp
@@ -5,8 +5,7 @@
 Smaller numbers will scroll slower, larger number will scroll faster. 1 will scroll at an equal rate to the main layer (presumable the player)."
   `(single-float 0.0001  10000.0))
 
-@export-class
-(defclass scene-background (static-sprite)
+(defclass parallax-image (static-sprite)
   ((horizontal-parallax :initarg :horizontal-parallax
                         :initform 1.0
                         :accessor horizontal-parallax
@@ -14,9 +13,35 @@ Smaller numbers will scroll slower, larger number will scroll faster. 1 will scr
    (vertical-parallax :initarg :vertical-parallax
                       :initform 1.0
                       :accessor vertical-parallax
-                      :documentation "y-axis scrolling factor"))
+                      :documentation "y-axis scrolling factor")))
+
+@export-class
+(defclass scene-background (aabb)
+  ((layers :initform nil))
   (:documentation "Image displayed behind a scene."))
 
+(defmethod initialize-instance :after ((background scene-background) &key layers)
+  (unless layers (error ":layers required"))
+  (setf
+   (slot-value background 'layers)
+   (loop :with parallax-images = (make-array 1 :fill-pointer 0 :adjustable T)
+      :for item :in layers :do
+      (when (stringp item)
+        (vector-push-extend (make-instance 'parallax-image
+                                           :width (width background)
+                                           :height (height background)
+                                           :path-to-image item)
+                            parallax-images))
+      :finally (return parallax-images))))
+
+(defmethod render ((background scene-background) update-percent camera rendering-context)
+  (with-slots (layers) background
+    ;; (format T "render layers! ~A~%" layers)
+    (loop :for layer :across layers :do
+       (render layer update-percent camera rendering-context))))
+
+;; TODO: apply this logic to parallax-image
+#+nil
 (defmethod world-to-screen-cords ((background scene-background) (camera simple-camera))
   (declare (optimize (speed 3)
                      (space 3))
