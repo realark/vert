388e888abd21aa9d0cde1d854efe76287ef63ec6
Ark <arktheprogrammer@gmail.com>
2019-10-18 10:44:17 -0700

    one direction option for direction-tracker

--- src/ai/direction-tracker.lisp
+++ src/ai/direction-tracker.lisp
@@ -5,31 +5,36 @@
 (defclass direction-tracker (agent obb)
   ((facing :initarg :facing
            :initform '(:EAST)
-           :reader facing))
+           :reader facing)
+   (simultanious-directions-p :initarg :simultainous-directions-p
+                              :initform t))
   (:documentation "An agent that tracks the direction it is facing"))
 
-(defgeneric push-direction (direction-tracker direction-keyword)
-  (:documentation "Face DIRECTION-TRACKER in the DIRECTION-KEYWORLD.
-Mutually exclusive dirs will be updated (e.g. pushing :east removes :west).")
-  (:method ((direction-tracker direction-tracker) direction)
-    (flet ((opposite-direction (direction)
-             (case direction
-               (:NORTH :SOUTH)
-               (:SOUTH :NORTH)
-               (:EAST :WEST)
-               (:WEST :EAST)
-               (otherwise nil))))
-      (with-slots ((facing facing)) direction-tracker
-        (unless (find direction facing)
-          (let ((opposite (opposite-direction direction)))
-            (when opposite
-              ;; having an opposite means we were given a valid direction
-              (setf facing
-                    (sort
-                     (append
-                      (remove opposite facing)
-                      (list direction))
-                     #'string-lessp)))))))))
+(defun push-direction (direction-tracker direction)
+  "Face DIRECTION-TRACKER towards DIRECTION. Mutually exclusive dirs will be updated (e.g. pushing :east removes :west)."
+  (declare (direction-tracker direction-tracker)
+           (keyword direction))
+  (flet ((opposite-direction (direction)
+           (case direction
+             (:NORTH :SOUTH)
+             (:SOUTH :NORTH)
+             (:EAST :WEST)
+             (:WEST :EAST)
+             (otherwise nil))))
+    (with-slots (facing simultanious-directions-p) direction-tracker
+      (unless (find direction facing)
+        (let ((opposite (opposite-direction direction)))
+          (if simultanious-directions-p
+              (when opposite
+                ;; having an opposite means we were given a valid direction
+                (setf facing
+                      (sort
+                       (append
+                        (remove opposite facing)
+                        (list direction))
+                       #'string-lessp)))
+              ;; only one direction is allowed so just replace the first element
+              (setf (first facing) direction)))))))
 
 (defgeneric is-facing (agent point)
   (:documentation "T if the agent is facing the given point")
