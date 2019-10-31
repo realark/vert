c0ba64cf489c346b80e7c9a051dbebbccebc8a2b
Ark <arktheprogrammer@gmail.com>
2019-10-31 10:56:40 -0700

    basic dialog showing api

--- src/plugins/dialog.lisp
+++ src/plugins/dialog.lisp
@@ -15,7 +15,10 @@
 ;;   (:documentation "Sound to play when DIALOG-SPEAKER is speaking. Maybe be nil."))
 
 (defclass dialog-hud (overlay input-handler)
-  ((window-width :initarg :window-width
+  ((show-p :initform nil)
+   (initiator :initform nil
+              :documentation "game-object to return input control to once this HUD closes.")
+   (window-width :initarg :window-width
                  :initform 100)
    (window-height :initarg :window-height
                   :initform 10)
@@ -51,3 +54,42 @@
               (height background) window-height
               (x background) (- (x text) window-padding)
               (y background) (- (y text) window-padding))))))
+
+(defmethod render ((hud dialog-hud) update-percent camera rendering-context)
+  (when (slot-value hud 'show-p)
+    (call-next-method hud update-percent camera rendering-context)))
+
+(defun advance-dialog (dialog-hud)
+  (declare (dialog-hud dialog-hud))
+  ;; TODO: advance to next block of dialog when one exists
+  (quit-dialog dialog-hud))
+
+(defun quit-dialog (dialog-hud)
+  (declare (dialog-hud dialog-hud))
+  (with-slots (show-p initiator) dialog-hud
+    (when initiator
+      (setf (active-input-device initiator) (active-input-device dialog-hud)))
+    (setf (active-input-device dialog-hud) *no-input-id*
+          show-p nil)))
+
+(defun show-dialog (dialog-hud text &key initiator)
+  (with-slots (show-p (hud-initiator initiator)) dialog-hud
+    (when show-p (error "dialog already shown"))
+    (when initiator
+      (setf (active-input-device dialog-hud) (active-input-device initiator)
+            (active-input-device initiator) *no-input-id*))
+    (setf hud-initiator initiator
+          (text (dialog-hud-text dialog-hud)) text
+          show-p t)))
+
+(set-default-input-command-map
+ dialog-hud
+ ("controller"
+  (:0 :advance-dialog))
+ ("sdl-keyboard"
+  (:scancode-space :advance-dialog)))
+
+(set-default-command-action-map
+ dialog-hud
+ (:advance-dialog
+  (on-activate (advance-dialog dialog-hud))))
