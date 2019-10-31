7cfe82068223da4e066daa5de52199b85df608ce
Ark <arktheprogrammer@gmail.com>
2019-10-31 10:22:12 -0700

    refactor dialog slots to focus on window height and padding
    Users will directly access the text font-drawable to change font,
    color, etc

--- src/plugins/dialog.lisp
+++ src/plugins/dialog.lisp
@@ -15,36 +15,39 @@
 ;;   (:documentation "Sound to play when DIALOG-SPEAKER is speaking. Maybe be nil."))
 
 (defclass dialog-hud (overlay input-handler)
-  ((text-width :initarg :text-width
-               :initform 100)
-   (text-height :initarg :text-height
-                :initform 10)
-   (border-padding :initarg :border-padding
-                   :initform 1)
-   (text :initarg :text-color
-         :initform nil)
+  ((window-width :initarg :window-width
+                 :initform 100)
+   (window-height :initarg :window-height
+                  :initform 10)
+   (window-padding :initarg :window-padding
+                   :initform 1
+                   :documentation "Guaranteed amount of space on all sides of the window which will not contain text.")
+   (text :initarg :text
+         :type font-drawable
+         :initform nil
+         :reader dialog-hud-text)
    (background :initarg :background
-               :initform nil)
-   (text-color :initform *black*)))
+               :initform nil)))
 
 (defmethod initialize-instance :after ((hud dialog-hud) &rest args)
   (declare (ignore args))
-  (with-slots (background border-padding text text-color text-width text-height) hud
-    (when background
-      ;; add background first so it renders behind the text
-      (setf (parent background) hud))
-    (setf text
-          (make-instance 'font-drawable
-                         :color text-color
-                         :parent hud
-                         :x (- (/ (width hud) 2.0)
-                               (/ text-width 2.0))
-                         :y 0
-                         :width text-width
-                         :height text-height
-                         :text "A man, a plan, a canal. Panama!"))
-    (when background
-      (setf (width background) (+ (width text) border-padding)
-            (height background) (+ (height text) border-padding)
-            (x background) (- (x text) border-padding)
-            (y background) (- (y text) border-padding)))))
+  (with-slots (window-width window-height window-padding text background) hud
+    (unless text
+      (error ":text required"))
+    (let ((text-width (- window-width (* 2 window-padding)))
+          (text-height (- window-height (* 2 window-padding))))
+      (when background
+        ;; add background first so it renders behind the text
+        (setf (parent background) hud))
+      (setf (parent text) hud
+            (x text) (+ (- (/ (width hud) 2.0)
+                           (/ text-width 2.0))
+                        window-padding)
+            (y text) window-padding
+            (width text) text-width
+            (height text) text-height)
+      (when background
+        (setf (width background) window-width
+              (height background) window-height
+              (x background) (- (x text) window-padding)
+              (y background) (- (y text) window-padding))))))
