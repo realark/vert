38479ed3c91c14e385c1ac788a162003effa507c
Ark <arktheprogrammer@gmail.com>
2020-04-06 16:38:36 -0700

    cache render matrix

--- src/graphics/drawable.lisp
+++ src/graphics/drawable.lisp
@@ -12,6 +12,8 @@
           :initform nil
           :documentation "A color mod blended (multiplied) with the drawable. Nil has the same effect as *white*."
           :accessor color)
+   (interpolator0-dirty-p :initform t)
+   (interpolator1-dirty-p :initform t)
    (interpolator :initform (make-matrix-interpolator)
                  :documentation "Holds the previous position of the game-object. Used for interpolating between update frames."))
   (:documentation "A game-object which can be rendered to the screen. Actual rendering is done by DRAW-COMPONENT slot."))
@@ -46,12 +48,26 @@
       (declare (dynamic-extent m))
       (interpolator-update interpolator m))))
 
+(defmethod mark-obb-dirty :after ((drawable drawable))
+  (setf (slot-value drawable 'interpolator0-dirty-p) t
+        (slot-value drawable 'interpolator1-dirty-p) t))
+
 (defmethod pre-update :before ((drawable drawable))
   (declare (optimize (speed 3)))
-  (with-slots (interpolator) drawable
-    (let ((m (obb-render-transform drawable)))
-      (declare (dynamic-extent m))
-      (interpolator-update interpolator m)))
+  (with-slots ((1dirty-p interpolator0-dirty-p)
+               (0dirty-p interpolator1-dirty-p))
+      drawable
+    (when (or 1dirty-p 0dirty-p)
+      (with-slots (interpolator) drawable
+        (let ((m (obb-render-transform drawable)))
+          (declare (dynamic-extent m))
+          (interpolator-update interpolator m)))
+      ;; it takes two passes to clean ourselves
+      (if 0dirty-p
+          ;; pass 1
+          (setf 0dirty-p nil)
+          ;; pass 2
+          (setf 1dirty-p nil))))
   (values))
 
 (defmethod recycle ((drawable drawable))
