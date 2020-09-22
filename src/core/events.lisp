5255bca0ddc2e9ab4c317fba84b19f98f1ad025c
Ark <arktheprogrammer@gmail.com>
2020-09-22 08:25:03 -0700

    new event system api and impl
    Not hooked into vert yet

---
+++ src/core/events.lisp
@@ -0,0 +1,240 @@
+;;;; A simple event system api
+(defpackage :vertdev ; TODO put in vert package
+  (:use #:cl #:bordeaux-threads))
+(in-package :vertdev)
+
+(defclass event-publisher ()
+  ((event-subscribers :documentation "event-name-symbol -> (weak) array of subs"))
+  (:documentation "An object which may publish events"))
+
+;; lazy initialize event map
+(defmethod slot-unbound (class (object event-publisher) (slot-name (eql 'event-subscribers)))
+  (setf (slot-value object 'event-subscribers) (make-hash-table :test #'eq)))
+
+;;; implementation
+
+(defclass %event ()
+  ((publisher :initarg :publisher
+              :initform (error ":publisher required"))
+   (name :initarg :name
+         :type symbol
+         :initform (error ":name required"))
+   (args :initarg :args
+         :initform (make-array 0 :adjustable t))))
+
+(defmethod print-object ((event %event) out)
+  (with-slots (publisher name args) event
+    (print-unreadable-object (event out :type t)
+      (format out "(~A -> ~A ~A)" publisher name args))))
+
+;;; %pending-events% and %event-bus% are swapped at the start of every update frame
+;;; and the new %pending-events% is truncated
+
+(declaim ((vector %event) %pending-events%))
+(defvar %pending-events%
+  (make-array 128 :fill-pointer 0 :adjustable t)
+  "Events which will be published next frame.")
+
+(declaim ((vector %event) %event-bus%))
+(defvar %event-bus%
+  (make-array 128 :fill-pointer 0 :adjustable t)
+  "Events which have been published in the previous update frame.")
+
+(declaim ((vector %event) %event-object-pool%))
+(defvar %event-object-pool%
+  (make-array 128 :fill-pointer 0 :adjustable t)
+  "Object pool for event instances to reduce GC caused by frequent events firing every frame.")
+
+@export
+(defmacro do-events ((event-name event-publisher event-args) &body body)
+  "Iterate all events published in the previous frame. Invoked RETURN to stop iteration.
+NOTE: frequent scans of the event bus can prove costly. Please consider using event handlers instead of this method."
+  (declare (symbol event-name event-publisher event-args))
+  (alexandria:with-gensyms (event-obj)
+    `(loop :for ,event-obj :across %event-bus% :do
+      (with-slots ((,event-publisher publisher) (,event-name name) (,event-args args)) ,event-obj
+        ,@body))))
+
+(defun events-run-pending ()
+  "Run events from the previous frame. Called at the beginning of an update frame.
+Do not call this function from game code. It will be invoked by the engine at the beginning of the update frame."
+  (declare (optimize (speed 3)))
+  (block swap-buffers-and-truncate-pending-events
+    (rotatef %event-bus% %pending-events%)
+    ;; explicitly null out old events and args so we don't hold a strong ref
+    ;; NOTE if this proves inefficient consider only doing this when the engine does an explicit GC
+    (loop :for old-event :across %pending-events% :do
+      (with-slots (name publisher args) old-event
+        (loop :for arg :across args :do
+          (setf arg nil))
+        (setf name 'recycled
+              publisher nil
+              (fill-pointer args) 0))
+      (vector-push-extend old-event %event-object-pool%)
+          :finally (setf (fill-pointer %pending-events%) 0)))
+
+  (do-events (name publisher args)
+    (%event-pub name publisher args)
+    (when (typep publisher 'event-publisher)
+      (with-slots ((subs event-subscribers)) publisher
+        (loop :for sub :across (the vector (gethash name subs #())) :do
+          (%event-sub name publisher sub args))))))
+
+;;;; Basic Event pub/sub api
+
+(defgeneric %event-pub (event-name publisher &optional event-args)
+  (:documentation "clos impl of event pub method dispatch. Event pub methods will specialize on this method")
+  (:method (event-name publisher &optional event-args)
+    (declare (optimize (speed 3))
+             (ignore event-name publisher event-args))
+    ;; no-op
+    (values)))
+
+(defgeneric %event-sub (event-name pub sub &optional event-args)
+  (:documentation "clos impl of event sub method dispatch. Event handler methods will specialize on this method")
+  (:method (event-name pub sub &optional event-args)
+    (declare (optimize (speed 3))
+             (ignore event-name pub sub event-args))))
+
+@export
+(defmacro defevent (event-name (publisher &rest event-args)
+                    &optional doc-string &body body)
+  "An action which runs once, before any subscriber handlers are invoked."
+  (declare ((or null string) doc-string)
+           (list event-args)
+           ((or symbol (cons symbol)) publisher)
+           (symbol event-name))
+  (assert (every #'symbolp event-args))
+  (when (listp publisher) (assert (= (length publisher) 2)))
+  `(progn
+     (unless (or (null ,doc-string)
+                 (documentation ',event-name 'variable))
+       (setf (documentation ',event-name 'variable)
+             ,doc-string))
+     (defmethod %event-pub ((event-name (eql ',event-name)) ,publisher &optional event-args)
+       (let (,@(loop :with i = -1
+                     :for arg :in event-args :collect
+                     `(,arg (elt event-args ,(incf i)))))
+         ,@body))
+     ',event-name))
+
+@export
+(defun event-publish (name publisher &rest args)
+  (declare (optimize (speed 3))
+           ((not null) publisher)
+           (symbol name)
+           (dynamic-extent args))
+  (let ((event ; recycle from obj pool, or make a new event instance
+          (cond ((> (length %event-object-pool%) 0)
+                 (vector-pop %event-object-pool%))
+                (t (make-instance '%event
+                                  :name nil
+                                  :publisher nil
+                                  :args (make-array (length args)
+                                                    :fill-pointer 0
+                                                    :adjustable t
+                                                    :initial-contents args))))))
+    (with-slots ((n name) (p publisher) (a args)) event
+      (vert::array-null-out a)
+      (setf n name
+            p publisher
+            (fill-pointer a) 0)
+      (loop :for arg :in args :do
+        (vector-push-extend arg a)))
+    (vector-push-extend event %pending-events%)))
+
+@export
+(defmacro defevent-handler (event-name (pub sub &rest event-args)
+                            &optional doc-string
+                            &body body)
+  "An action which runs once per subscriber to an event"
+  (declare (symbol event-name)
+           ((or null string) doc-string)
+           ((or symbol (cons symbol)) pub sub)
+           (list event-args))
+  (assert (every #'symbolp event-args))
+  (when (listp pub) (assert (= (length pub) 2)))
+  (when (listp sub) (assert (= (length sub) 2)))
+  `(progn
+     (unless (or (null ,doc-string)
+                 (documentation ',event-name 'variable))
+       (setf (documentation ',event-name 'variable)
+             ,doc-string))
+     (defmethod %event-sub ((event-name (eql ',event-name)) ,pub ,sub &optional event-args)
+       (let (,@(loop :with i = -1
+                     :for arg :in event-args :collect
+                     `(,arg (elt event-args ,(incf i)))))
+         ,@body))
+
+     ',event-name))
+
+@export
+(defmacro event-subscribe (publisher subscriber &rest event-names)
+  "Notify SUBSCRIBER of PUBLISHER's events specified by EVENT-NAMES."
+  (assert (every #'symbolp event-names))
+  (alexandria:once-only (publisher subscriber)
+    `(with-slots (event-subscribers) ,publisher
+       (loop :for event-name :across #(,@event-names) :do
+            (let ((sub-list (gethash event-name event-subscribers)))
+              (unless sub-list
+                (setf sub-list (sb-ext:make-weak-vector 1 :initial-element nil)
+                      (gethash event-name event-subscribers) sub-list))
+              (locally (declare ((simple-array) sub-list))
+                (loop :with first-null-index = nil
+                   :for i :from 0 :below (length sub-list) :do
+                     (let ((sub (elt sub-list i)))
+                       (cond ((and (null first-null-index)
+                                   (null sub))
+                              (setf first-null-index i))
+                             ((eq sub ,subscriber)
+                              ;; already in sub list. Stop.
+                              (return))))
+                   :finally
+                   ;; not found in sub list
+                     (unless first-null-index
+                       ;; resize list if no empty spaces
+                       (setf first-null-index (length sub-list)
+                             sub-list (recurse.vert::simple-array-double-size
+                                       sub-list
+                                       :new-array (sb-ext:make-weak-vector (* 2 (length sub-list))
+                                                                           :initial-element nil))
+                             (gethash event-name event-subscribers) sub-list))
+                     (setf (elt sub-list first-null-index)
+                           ,subscriber))))))))
+
+@export
+(defmacro event-remove (publisher subscriber &rest event-names)
+  "Stop notifying SUBSCRIBER of PUBLISHER's events."
+  (assert (every #'symbolp event-names))
+  `(with-slots (event-subscribers) ,publisher
+     (loop :for event-name :across #(,@event-names) :do
+       (let ((sub-list (gethash event-name event-subscribers)))
+         (when sub-list
+           (locally (declare ((simple-array) sub-list))
+             (loop :for i :from 0 :below (length sub-list) :do
+               (when (eq ,subscriber (elt sub-list i))
+                 (setf (elt sub-list i) nil)
+                 (return)))))))))
+
+;;;; Global Subscribers
+
+@export
+(defmacro defevent-handler-global (global-handler-name (event-name publisher &rest event-args) &optional doc-string &body body)
+  "Define a global handler to run after specific event name and publisher types"
+  (declare (symbol global-handler-name event-name)
+           ((or symbol (cons symbol)) publisher)
+           (list event-args)
+           ((or null string) doc-string))
+  (assert (every #'symbolp event-args))
+  (when (listp publisher) (assert (= (length publisher) 2)))
+  `(progn
+     (unless (or (null ,doc-string)
+                 (documentation ',global-handler-name 'variable))
+       (setf (documentation ',global-handler-name 'variable)
+             ,doc-string))
+     (defmethod %event-pub :after ((event-name (eql ',event-name)) ,publisher &optional event-args)
+       (let (,@(loop :with i = -1
+                     :for arg :in event-args :collect
+                     `(,arg (elt event-args ,(incf i)))))
+         ,@body))
+     ',global-handler-name))
---
+++ t/events-test.lisp
@@ -0,0 +1,158 @@
+(in-package :vertdev) ; TODO put in vert package
+;; (in-package :recurse.vert)
+
+(defparameter %test-thing-pub-body-invokes% (list))
+(defparameter %test-thing-sub-body-invokes% (list))
+(defparameter %test-thing-global-invokes% (list))
+
+(defclass test-pub (event-publisher) ())
+
+(defclass test-sub () ())
+
+(defevent test-thing (pub arg1 arg2)
+    "A test event"
+  (push (list pub arg1 arg2)
+        %test-thing-pub-body-invokes%))
+
+(defevent-handler test-thing ((pub test-pub) (sub test-sub) arg1 arg2)
+    "Some doc"
+  (push (list pub sub arg1 arg2)
+        %test-thing-sub-body-invokes%))
+
+(defevent-handler-global global-test-thing (test-thing pub arg1 arg2)
+    "Some doc"
+  (push (list pub arg1 arg2)
+        %test-thing-global-invokes%))
+
+(prove:deftest events-do-not-cons
+  (block warm-up
+    (loop :for i :from 1 :below 1024 :do
+      (event-publish 'input-happened
+                     *standard-input*
+                     'arg-one
+                     'arg-two
+                     'arg-three
+                     'arg-four
+                     'arg-five)
+          :finally
+             (events-run-pending)))
+  (sb-ext:gc :full t)
+
+  (loop :with pub-size = 256
+        :and starting-heap-size = (sb-kernel:dynamic-usage)
+        :and test-cons-max-bytes = 10
+        :and starting-gc-count = (vert::current-gc-count)
+        :for i :from 1 :below 300000 :do
+          (event-publish 'input-happened
+                         *standard-input*
+                         'arg-one
+                         'arg-two
+                         'arg-three
+                         'arg-four
+                         'arg-five)
+          (when (and (> i 0) (= (mod i pub-size) 0))
+            (assert (= (length %pending-events%) pub-size))
+            (events-run-pending)
+            (assert (= (length %pending-events%) 0)))
+        :finally
+           (prove:is (vert::current-gc-count) starting-gc-count
+                     "No Garbage Collection occurred.")
+           (prove:is (- (sb-kernel:dynamic-usage) starting-heap-size)
+                     test-cons-max-bytes
+                     :test #'<=
+                     (format t "No more than ~A bytes consed." test-cons-max-bytes))))
+
+(prove:deftest events-low-level
+  (events-run-pending)
+
+  (let ((%test-thing-pub-body-invokes% (list))
+        (%test-thing-sub-body-invokes% (list))
+        (%test-thing-global-invokes% (list))
+        (sub1 (make-instance 'test-sub))
+        (sub2 (make-instance 'test-sub))
+        (sub3 (make-instance 'test-sub))
+        (pub (make-instance 'test-pub))
+        (num-events 100))
+    (event-subscribe pub sub1 test-thing)
+    (event-subscribe pub sub2 test-thing)
+    (event-subscribe pub sub3 test-thing some-other-event)
+    (prove:is (slot-value pub 'event-subscribers)
+              (%make-hash (list 'test-thing (vector sub1 sub2 sub3)
+                                'some-other-event (vector sub3)))
+              :test #'%hash-tables-equalp
+              "All subs in subscriber list")
+    (event-remove pub sub3 test-thing)
+    (prove:is (slot-value pub 'event-subscribers)
+              (%make-hash (list 'test-thing (vector sub1 sub2)
+                                'some-other-event (vector sub3)))
+              :test #'%hash-tables-equalp
+              "sub3 removed from test-thing event")
+    ;; (event-subscribe pub sub3 test-thing some-other-event)
+    ;; (event-remove pub sub3 test-thing)
+    (loop :for i :from 0 :below num-events :do
+      (event-publish 'test-thing pub 1 2))
+    (events-run-pending)
+
+    (prove:is (length %test-thing-pub-body-invokes%) 100)
+    (prove:is (first %test-thing-pub-body-invokes%)
+              (list pub 1 2)
+              :test #'equalp)
+    (prove:is %test-thing-pub-body-invokes%
+              (make-list num-events
+                         :initial-element
+                         (list pub 1 2))
+              :test #'equalp)
+    (prove:is (length %test-thing-sub-body-invokes%) (* 2 num-events)
+              "Each subscribed sub receives event handler callback.")
+
+    ;; expecting the sub invoke list to alternate between sub1 and sub2
+    (loop :with expected-sub = (second (first %test-thing-sub-body-invokes%))
+          :for i :from 0
+          :for invoke :in  %test-thing-sub-body-invokes% :do
+            (unless (eq (second invoke) expected-sub)
+              (prove:fail
+               (format nil "~A :: (1: ~A, 2: ~A) expected to find sub ~A, but found ~A instead"
+                       i sub1 sub2
+                       expected-sub
+                       (second invoke)))
+              (return))
+            (log:trace "~A (1: ~A, 2: ~A) Found expected sub: ~A"
+                       i sub1 sub2
+                       expected-sub)
+            (if (eq sub1 expected-sub)
+                (setf expected-sub sub2)
+                (setf expected-sub sub1))
+          :finally
+             (prove:pass "Subs invoked in expected order."))
+
+    (prove:is %test-thing-global-invokes% %test-thing-pub-body-invokes%
+              :test #'equalp
+              ;; global handler body produces the same list structure as the pub list
+              "Global handler invoked")))
+
+(defun %make-hash (plist)
+  "Make a hash table out of PLIST"
+  (let ((h (make-hash-table :test #'eq)))
+    (loop :for (k v) :on plist :by #'cddr :while v :do
+      (setf (gethash k h) v))
+    h))
+
+(defun %hash-tables-equalp (hash1 hash2)
+  (and (= (hash-table-count hash1)
+          (hash-table-count hash2))
+       (loop :for key1 :being :the hash-keys :of hash1
+               :using (hash-value value1) :do
+                 ;; remove impl null padding
+                 (let ((value1 (remove-if #'null value1))
+                       (value2 (remove-if #'null (gethash key1 hash2 #()))))
+                   (unless (and (= (length value1)
+                                   (length value2))
+                                (loop :for v1 :across value1
+                                      :for v2 :across value2 :do
+                                        (unless (equalp v1 v2)
+                                          (return nil))
+                                      :finally
+                                         (return t)))
+                     (return nil)))
+             :finally
+                (return t))))
